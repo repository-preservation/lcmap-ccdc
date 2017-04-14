93591a936c1e721ad063cb458e87ec54d049dfa5
davidvhill <davehill75@gmail.com>
2017-04-13 19:47:48 -0500

    removed hy code, it isnt mature enough and several key functions do not work.  aardvark.py is a little better than 1/2 done.  WIP

--- firebird/__init__.py
+++ firebird/__init__.py
@@ -1,20 +1,15 @@
-CASSANDRA_HOST = ''
-CASSANDRA_PORT = ''
-CASSANDRA_USER = ''
-CASSANDRA_PW   = '' 
-
-INPUTS_DIR  = ''
-OUTPUTS_DIR = ''
+CASSANDRA_CONTACT_POINTS = os.getenv('CASSANDRA_CONTACT_POINTS', '0.0.0.0')
+CASSANDRA_USER = os.getenv('CASSANDRA_USER')
+CASSANDRA_PASS   = os.getenv('CASSANDRA_PASS')
 
 LOG_LEVEL = ''
 
-AMQP_EXCHANGE    = ''
-AMQP_QUEUE       = ''
-AMQP_ROUTING_KEY = ''
+SPARK_MASTER = os.getenv('SPARK_MASTER', 'spark://localhost:7077')
 
-SPARK_MASTER = ''
+AARDVARK_HOST = os.getenv('AARDVARK_HOST', 'localhost')
+AARDVARK_PORT = os.getenv('AARDVARK_PORT', '5678')
+AARDVARK_TILESPECS = os.getenv('AARDVARK_TILESPECS', '/landsat/tile-specs')
 
 def minbox(points):
     """ Returns the minimal bounding box necessary to contain points """
     pass
-     
