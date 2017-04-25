9af663ae99b61c04f2937829373e667a273add13
davidvhill <davehill75@gmail.com>
2017-04-25 15:59:19 -0500

    added flatten and files.py module

--- test/fixtures.py
+++ test/fixtures.py
@@ -3,23 +3,34 @@ import glob
 import json
 import os
 import itertools
+from firebird.files import read
 
-def read(path):
-    with open(path, 'r+') as handle:
-        return handle.read()
+def flatten(iterable):
+    """
+    Reduce dimensionality of iterable containing iterables
+    :param iterable: A multi-dimensional iterable
+    :returns: A one dimensional iterable
+    """
+    return itertools.chain.from_iterable(iterable)
 
 def chips(spectra, root_dir='./resources/data/chips/band-json'):
+    """
+    Return chips for named spectra
+    :param spectra: red, green, blue, nir, swir1, swir2, thermal or cfmask
+    :type spectra: string
+    :returns: sequence of chips
+    """
     path = ''.join([root_dir, os.sep, '*', spectra, '*'])
     filenames = glob.glob(path)
     chips = [json.loads(read(filename)) for filename in filenames]
-    return itertools.chain.from_iterable(chips)
+    return flatten(chips)
 
 def chip_specs(spectra, root_dir='./resources/data/chip-specs'):
-    """ Returns chip specs for the named spectra.
-        Args:
-            spectra: red, green, blue, nir, swir1, swir2, thermal or cfmask
-        Returns:
-            Chip specs
+    """
+    Returns chip specs for the named spectra.
+    :param spectra: red, green, blue, nir, swir1, swir2, thermal or cfmask
+    :type spectra: string
+    :returns: sequence of chip specs
     """
     path = ''.join([root_dir, os.sep, '*', spectra, '*'])
     filenames = glob.glob(path)
