0f65365814125fc83e92d6c642e880adbe110030
clay austin <clay.austin.ctr@usgs.gov>
2017-04-27 11:51:46 -0500

    fix cassandra test for new module behavior with prepared_statements
    Former-commit-id: 8b5978822e05d51d0fdb5a842a9dbc4e89dbb95d

--- test/test_cassandra.py
+++ test/test_cassandra.py
@@ -1,11 +1,13 @@
-from firebird import cassandra
+from firebird.cassandra import execute, INSERT_CQL
+from datetime import datetime
+
+_d = datetime.strptime('2014-07-30T06:51:36Z', '%Y-%m-%dT%H:%M:%SZ')
 
 
 def test_save_detect():
     # in travis-ci, cant yet connect to container running cassandra
-    results = {'y': 999888, 'result_ok': True, 'algorithm': 'pyccd-1.1.0', 'inputs_md5': 'xoxoxo', 'tile_x':-123456,
-               'result': 'a pile of result', 'result_produced': '2014-07-30T06:51:36Z', 'result_md5': 'result_md5',
-               'x': -134567, 'tile_y': 888999}
-    # TODO: reenable this, commented it out per CA
-    #assert cassandra.save_pyccd_result(results)
-    assert True
+    results = {'y': 999888, 'result_ok': True, 'algorithm': 'pyccd-1.1.0', 'inputs_md5': 'xoxoxo', 'chip_x':-123456,
+               'result': 'a pile of result', 'result_produced': _d, 'result_md5': 'result_md5',
+               'x': -134567, 'chip_y': 888999}
+    assert execute(INSERT_CQL, [results])
+
