571fd70fce14e1eed4134ff260328daaee7b248e
davidvhill <davehill75@gmail.com>
2017-04-27 09:53:37 -0500

    updates for chips instead of tiles

--- test/test_cassandra.py
+++ test/test_cassandra.py
@@ -3,9 +3,9 @@ from firebird import cassandra
 
 def test_save_detect():
     # in travis-ci, cant yet connect to container running cassandra
-    results = {'y': 999888, 'result_ok': True, 'algorithm': 'pyccd-1.1.0', 'inputs_md5': 'xoxoxo', 'tile_x':-123456,
+    results = {'y': 999888, 'result_ok': True, 'algorithm': 'pyccd-1.1.0', 'inputs_md5': 'xoxoxo', 'chip_x':-123456,
                'result': 'a pile of result', 'result_produced': '2014-07-30T06:51:36Z', 'result_md5': 'result_md5',
-               'x': -134567, 'tile_y': 888999}
+               'x': -134567, 'chip_y': 888999}
     # TODO: reenable this, commented it out per CA
     #assert cassandra.save_pyccd_result(results)
     assert True
