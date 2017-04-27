fe096ade47939e0c758d759d5c353bce60d0a7ba
clay austin <clay.austin.ctr@usgs.gov>
2017-04-27 09:39:19 -0500

    replace tile references with chip
    Former-commit-id: 2bad7a416000ae9069b2fae09f1dd1faa704dad4

--- firebird/cassandra.py
+++ firebird/cassandra.py
@@ -5,6 +5,7 @@ from firebird import CASSANDRA_CONTACT_POINTS
 from firebird import CASSANDRA_USER
 from firebird import CASSANDRA_PASS
 from firebird import CASSANDRA_KEYSPACE
+from firebird import CASSANDRA_RESULTS_TABLE
 
 auth_provider = PlainTextAuthProvider(username=CASSANDRA_USER, password=CASSANDRA_PASS)
 cluster = Cluster(CASSANDRA_CONTACT_POINTS.split(','), auth_provider=auth_provider)
@@ -12,8 +13,8 @@ cluster = Cluster(CASSANDRA_CONTACT_POINTS.split(','), auth_provider=auth_provid
 RESULT_INPUT = {'tile_x': '', 'tile_y': '', 'x': '', 'y': '', 'algorithm': '', 'result': '', 'result_ok': '',
                 'result_produced': '', 'inputs_md5': '', 'result_md5': ''}
 
-INSERT_CQL = "INSERT INTO {}.results (y, tile_x, tile_y, algorithm, x, result_ok, inputs_md5, result, " \
-             "result_produced, result_md5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(CASSANDRA_KEYSPACE)
+INSERT_CQL = "INSERT INTO {}.{} (y, tile_x, tile_y, algorithm, x, result_ok, inputs_md5, result, result_produced, " \
+             "result_md5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)".format(CASSANDRA_KEYSPACE, CASSANDRA_RESULTS_TABLE)
 
 
 def execute(statement, args):
