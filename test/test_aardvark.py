e17e9946bb8a959a1c0e0f470521c532e2233cf6
davidvhill <david.hill.ctr@usgs.gov>
2017-05-06 13:25:32 -0500

    all unit tests for aardvark are filled in and working.  renamed assoc to locrods as the generalization was a bit too abstract.  everything is now tested.  only thing left is to examine the module structure and make sure functions live in the right places, and then construct some example usages of the module for getting data.
    Former-commit-id: deb0a59a811d90141de6bebe69ce26064d26cf11

--- test/test_aardvark.py
+++ test/test_aardvark.py
@@ -7,11 +7,13 @@ from firebird.aardvark import intersection
 from firebird.aardvark import trim
 from firebird.aardvark import to_numpy
 from firebird.aardvark import rods
+from firebird.aardvark import locrods
 from fixtures import chip_specs
 from fixtures import flatten
 
 from base64 import b64encode
 from functools import reduce
+from functools import partial
 from itertools import product
 from hypothesis import given
 import hypothesis.strategies as st
@@ -107,6 +109,12 @@ def test_to_numpy():
     def _spec(dtype, shape, ubid):
         return {'ubid': ubid, 'data_shape': shape, 'data_type': dtype.upper()}
 
+    def _check(npchip, specs_byubid):
+        spec = specs_byubid[npchip['ubid']]
+        assert npchip['data'].dtype.name == spec['data_type'].lower()
+        assert npchip['data'].shape == spec['data_shape']
+        return True
+
     # Test combos of dtypes/shapes to ensure data shape and type are unaltered
     combos = tuple(product(('uint8', 'uint16', 'int8', 'int16'),
                            ((3, 3), (1, 1), (100, 100))))
@@ -116,13 +124,9 @@ def test_to_numpy():
     specs = [_spec(*c, _ubid(*c)) for c in combos]
     specs_byubid = byubid(specs)
 
-    # make assertions about results
-    for npchip in to_numpy(chips, specs_byubid):
-        ubid = npchip['ubid']
-        spec = specs_byubid[ubid]
-
-        assert npchip['data'].dtype.name == spec['data_type'].lower()
-        assert npchip['data'].shape == spec['data_shape']
+    # run assertions
+    checker = partial(_check, specs_byubid=specs_byubid)
+    all(map(checker, to_numpy(chips, specs_byubid)))
 
 
 def test_rods():
@@ -141,7 +145,7 @@ def test_rods():
     assert pillar.shape[1] == chips[0]['data'].shape[1]
     assert pillar.shape[2] == len(chips)
 
-    # going to flatten both chips arrays and the pillar array
+    # going to flatten both the chips arrays and the pillar array
     # then perform black magic and verify that the values wound up
     # where they belonged in the array positions.
     # using old style imperative code as double insurance
@@ -151,8 +155,8 @@ def test_rods():
     # We're mapping array locations between two things like that look like this:
     # [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 ...]
     # [11, 21, 31, 12, 22, 32, 13, 23, 33, 14, 24, 34, 15, 25, 35, 16, 26 ...]
-    # This alone serves as all the evidence needed to prove that imperative
-    # programming is bad for everyone involved.  I am very sorry.
+    # This alone should serve as all the evidence needed to prove that
+    # imperative programming is bad for everyone involved.  I am very sorry.
     fchips = list(flatten([c['data'].flatten() for c in chips]))
     jump = reduce(lambda accum, v: accum + v, pillar.shape) # 9
     modulus = pillar.shape[0] # 3
@@ -161,5 +165,24 @@ def test_rods():
         group = i // modulus # 0 1 2
         assert val == fchips[(factor * jump) + group]
 
-def test_assoc():
-    pass
+
+def test_locrods():
+    # test data.  sum(locs) + 2 should equal sum(rods) per inner array
+    # value of sum of every inner array element should be unique.
+    locs = np.int_([[[0, 0], [0, 1], [0, 2]],
+                    [[1, 3], [1, 4], [1, 5]],
+                    [[2, 6], [2, 7], [2, 8]]])
+
+    rods = np.int_([[[0, 0, 2], [0, 0, 3], [0, 0, 4]],
+                    [[1, 0, 5],  [1, 0, 6], [1, 0, 7]],
+                    [[2, 0, 8],  [2, 0, 9], [2, 0, 10]]])
+
+    # sanity check
+    locs_total = locs.reshape(9, 2).sum(axis=1) + 2
+    rods_total = rods.reshape(9, 3).sum(axis=1)
+    assert np.array_equal(locs_total, rods_total)
+
+    # make sure we located all the rods correctly.
+    flatlocs = locs.reshape(9, 2)
+    tseries = locrods(locs, rods)
+    assert all([tseries[tuple(loc)].sum() == loc.sum() + 2 for loc in flatlocs])
