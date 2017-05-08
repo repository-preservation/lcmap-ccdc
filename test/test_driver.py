e759e73ed9da6183f42d2ed48cd86aa1f1022140
clay austin <clay.austin.ctr@usgs.gov>
2017-05-08 07:39:24 -0500

    aardvark interactions with daves working branch

---
+++ test/test_driver.py
@@ -0,0 +1,19 @@
+from firebird import driver
+
+from test.shared import simplified_detect_results
+
+
+def test_simplify_detect_results():
+    assert True
+
+
+def test_detect():
+    assert True
+
+
+def test_assemble_ccd_data():
+    assert True
+
+
+def test_run():
+    assert True
