1c9370c4d0825e2451cd811579f682c096ab9dc9
davidvhill <david.hill.ctr@usgs.gov>
2017-05-11 15:41:18 -0500

    merging up test_cassandra from develop
    Former-commit-id: 2d6d26bfc0cec1642c581080b8a2751e8a6e92c7

--- test/test_cassandra.py
+++ test/test_cassandra.py
@@ -10,3 +10,4 @@ def test_save_detect():
                'result': 'a pile of result', 'result_produced': _d, 'result_md5': 'result_md5',
                'x': -134567, 'chip_y': 888999}
     assert execute(INSERT_CQL, [results])
+
