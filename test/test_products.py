6620cc736158a41cb96eb4fb6c717cb5abc2cf15
davidvhill <david.hill.ctr@usgs.gov>
2017-05-11 12:40:37 -0500

    stubbed out missing tests.  fixed misnamed rods to locrods in driver.  This is ready for a pr.
    Former-commit-id: 1a294505b026438206f6b72c886300807838d85f

---
+++ test/test_products.py
@@ -0,0 +1,29 @@
+from firebird import products
+
+
+def test_result_to_models():
+    assert 1 > 0
+
+
+def test_lastchange():
+    assert 1 > 0
+
+
+def test_changemag():
+    assert 1 > 0
+
+
+def test_changedate():
+    assert 1 > 0
+
+
+def test_seglength():
+    assert 1 > 0
+
+
+def test_qa():
+    assert 1 > 0
+
+
+def test_run():
+    assert 1 > 0
