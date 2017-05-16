cac0bfa85b1a24fbbbe582c80d119e1f30ef1aec
clay austin <clay.austin.ctr@usgs.gov>
2017-05-16 08:58:38 -0500

    WIP aardvark driver reconciliation

--- firebird/cmdline.py
+++ firebird/cmdline.py
@@ -5,7 +5,7 @@ from firebird import driver
 
 parser = argparse.ArgumentParser(description="Driver for LCMAP product generation")
 # could get cute here and loop this, leaving unwound for now
-parser.add_argument('-ord', dest='ord_date')
+parser.add_argument('-prd', dest='prod_date')
 parser.add_argument('-acq', dest='acquired')
 parser.add_argument('-ulx', dest='ulx')
 parser.add_argument('-uly', dest='uly')
@@ -26,9 +26,9 @@ def cli():
 
 
 @cli.command
-def run(acquired, ulx, uly, lrx, lry, ord_date,
+def run(acquired, ulx, uly, lrx, lry, prod_date,
         lastchange=False, changemag=False, changedate=False, seglength=False, qa=False):
-    return driver.run(acquired, ulx, uly, lrx, lry, ord_date, lastchange, changemag, changedate, seglength, qa)
+    return driver.run(acquired, ulx, uly, lrx, lry, prod_date, lastchange, changemag, changedate, seglength, qa)
 
 
 @cli.command
@@ -37,5 +37,5 @@ def products():
 
 
 if __name__ == "__main__":
-    run(args.acquired, args.ulx, args.uly, args.lrx, args.lry, args.ord_date,
+    run(args.acquired, args.ulx, args.uly, args.lrx, args.lry, args.prod_date,
         args.lastchange, args.changemag, args.changedate, args.seglength, args.qa)
