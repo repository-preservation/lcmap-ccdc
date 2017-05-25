ce9dee7c15b1ba91042aeb7a68754ee19228d7a2
clay austin <clay.austin.ctr@usgs.gov>
2017-05-25 15:03:00 -0500

    product tests WIP

--- test/test_products.py
+++ test/test_products.py
@@ -1,21 +1,33 @@
 from firebird import products
-
+from firebird import dtstr_to_ordinal as dto
 
 def test_result_to_models():
-    assert 1 > 0
+    _res = {'result': '{"change_models": "foo bar"}'}
+    assert products.result_to_models(_res) == "foo bar"
 
 
 def test_lastchange():
-    assert 1 > 0
+    inmodels = [{'change_probability': 1, 'break_day': 999}]
+    ord_date = 1000
+    _res = products.lastchange(inmodels, ord_date)
+    assert _res is 1
 
 
 def test_changemag():
-    assert 1 > 0
+    pick_a_day = "1984-04-01"
+    ord_date = dto(pick_a_day)
+    inmodels = [{'change_probability': 1, 'break_day': ord_date}]
+    _res = products.changemag(inmodels, ord_date)
+    assert _res is 0.0
 
 
 def test_changedate():
-    assert 1 > 0
-
+    pick_a_day = "1984-04-01"
+    ord_date = dto(pick_a_day)
+    inmodels = [{'change_probability': 1, 'break_day': ord_date}]
+    _res = products.changedate(inmodels, ord_date)
+    assert _res is 92
+    
 
 def test_seglength():
     assert 1 > 0
