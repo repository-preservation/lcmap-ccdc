9ec91a9b69e0061b7bc53e0b38d32edeacd5466f
davidvhill <david.hill.ctr@usgs.gov>
2017-06-04 12:53:04 -0500

    WIP, getting save function written now

--- firebird/cmdline.py
+++ firebird/cmdline.py
@@ -15,13 +15,29 @@ def products():
 
 
 @fbcmd.command()
+@c.option('--directory', '-d')
+@c.option('--iwds', '-i')
+@c.option('--fresh', '-f', is_flag=True)
+def train(from_iwds, acquired, bounds, products, product_dates):
+    pass
+
+
+@fbcmd.command()
 @c.option('--acquired', '-a')
 @c.option('--bounds', '-b', multiple=True)
 @c.option('--clip', '-c', is_flag=True)
+@c.option('--directory', '-d')
+@c.option('--iwds', '-i', is_flag=True)
 @c.option('--products', '-p', type=c.Choice(available_products, multiple=True)
 @c.option('--product_dates', '-d', multiple=True)
-def run(acquired, bounds, product, product_dates, clip):
-    return driver.run(acquired, bounds, products, product_dates, clip)
+def save(acquired, bounds, clip, directory, iwds, product, product_dates):
+    return driver.save(acquired=acquired,
+                       bounds=bounds,
+                       clip=clip,
+                       directory=directory,
+                       iwds=iwds,
+                       products=products,
+                       product_dates=product_dates)
 
 
 if __name__ == "__main__":
