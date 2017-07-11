90be4878fbb6e0dbf015a24d630bd478e60a450e
davidvhill <david.hill.ctr@usgs.gov>
2017-07-11 16:28:57 -0500

    WIP for running Firebird on Mesos

--- firebird/cmdline.py
+++ firebird/cmdline.py
@@ -1,10 +1,8 @@
-from firebird import aardvark as a
 from firebird import actions
+from firebird import chip_specs
+from firebird import transforms
 import click as c
 import firebird as fb
-
-
-products = fb.available_products()
 
 
 @c.group()
@@ -14,7 +12,10 @@ def fbcmd():
 
 @fbcmd.command()
 def products():
-    return fb.available_products()
+    """Returns a sequence of the products that may be requested from firebird
+    :return: Sequence of product names
+    """
+    return transforms.labels().keys()
 
 
 @fbcmd.command()
@@ -33,46 +34,8 @@ def count(bounds, product):
 @fbcmd.command()
 @c.option('--bounds', '-b')
 def chipids(bounds):
-    spec = a.chip_spec(d.chip_spec_queries(fb.SPECS_URL)['blues'])[0]
+    spec = chip_specs.get(d.chip_spec_queries(fb.SPECS_URL)['blues'])[0]
     return chip.ids(fb.minbox(bounds), spec)
-
-
-@fbcmd.command()
-@c.option('--bounds', '-b', multiple=True)
-@c.option('--product', '-p')
-def missing(bounds, product):
-    """Returns ids that do not exist within the given bounds"""
-    #  sc = fb.SparkContext()
-    #  ss = sql.SparkSession(ss)
-    #  ss.createDataFrame(fb.minbox(bounds).xys())
-    #  ss.product.load()
-    #  return bounds.subtract(product.load().xys())
-    pass
-
-
-@fbcmd.command()
-@c.option('--directory', '-d')
-@c.option('--iwds', '-i')
-@c.option('--fresh', '-f', is_flag=True)
-def train(from_iwds, acquired, bounds, products, product_dates):
-    pass
-
-
-@fbcmd.command()
-@c.option('--acquired', '-a')
-@c.option('--bounds', '-b', multiple=True)
-@c.option('--clip', '-c', is_flag=True)
-@c.option('--directory', '-d')
-@c.option('--products', '-p', type=c.Choice(fb.available_products(),
-          multiple=True)
-@c.option('--product_dates', '-d', multiple=True)
-def evaluate(acquired, bounds, clip, directory, product, product_dates):
-    return actions.evaluate(acquired=acquired,
-                            bounds=bounds,
-                            clip=clip,
-                            directory=directory,
-                            products=products,
-                            product_dates=product_dates)
 
 
 @fbcmd.command()
@@ -86,7 +49,7 @@ def save(acquired, bounds, clip, product, product_dates):
     return actions.save(acquired=acquired,
                         bounds=bounds,
                         clip=clip,
-                        products=products,
+                        products=products(),
                         product_dates=product_dates)
 
 
