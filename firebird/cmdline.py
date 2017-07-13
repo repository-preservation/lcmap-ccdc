b3ff6e233353a135bc924fe236d7b39d973b5d28
davidvhill <davehill75@gmail.com>
2017-07-13 17:19:25 -0500

    working firebird install.  WIP
    Former-commit-id: 8ff93b049d09e24435f5cf1a51d04978ab6a1724

--- firebird/cmdline.py
+++ firebird/cmdline.py
@@ -1,26 +1,48 @@
 from firebird import actions
 from firebird import chip_specs
+from firebird import files
 from firebird import transforms
 import click as c
 import firebird as fb
+import os
 
 
-@c.group()
-def fbcmd():
+def list_products():
+    return list(transforms.labels().keys())
+
+
+def context_settings():
+    return dict(token_normalize_func=lambda x: x.lower())
+
+
+@c.group(context_settings=context_settings())
+def cli():
     pass
 
 
-@fbcmd.command()
+@cli.group()
+def show():
+    pass
+
+
+@show.command()
 def products():
     """Returns a sequence of the products that may be requested from firebird
     :return: Sequence of product names
     """
-    return transforms.labels().keys()
+    c.echo(list_products())
 
 
-@fbcmd.command()
-@c.option('--bounds', '-b', multiple=True)
-@c.option('--product', '-p')
+@show.command()
+def algorithms():
+    c.echo((os.path.exists('/algorithms') and
+            os.path.isdir('/algorithms') and
+            os.listdir('/algorithms')) or "No algorithms available.")
+
+
+@cli.command()
+@c.argument('--bounds', '-b')
+@c.argument('--product', '-p')
 def count(bounds, product):
     """Returns count of number of results given bounds and a product.  Bounds
     will be minbox'd prior to calculation.
@@ -31,18 +53,18 @@ def count(bounds, product):
     pass
 
 
-@fbcmd.command()
-@c.option('--bounds', '-b')
+@show.command()
+@c.argument('--bounds', '-b')
 def chipids(bounds):
     spec = chip_specs.get(d.chip_spec_queries(fb.SPECS_URL)['blues'])[0]
     return chip.ids(fb.minbox(bounds), spec)
 
 
-@fbcmd.command()
+@cli.command()
 @c.option('--acquired', '-a')
 @c.option('--bounds', '-b', multiple=True)
 @c.option('--clip', '-c', is_flag=True)
-@c.option('--products', '-p', type=c.Choice(fb.available_products(),
+@c.option('--products', '-p', type=c.Choice(list_products()),
           multiple=True)
 @c.option('--product_dates', '-d', multiple=True)
 def save(acquired, bounds, clip, product, product_dates):
@@ -54,4 +76,4 @@ def save(acquired, bounds, clip, product, product_dates):
 
 
 if __name__ == "__main__":
-    run()
+    cli()
