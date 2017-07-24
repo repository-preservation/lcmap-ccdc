168ee9dda85fd84ae86d353ce077856112074463
davidvhill <david.hill.ctr@usgs.gov>
2017-07-24 15:35:29 -0500

    validation code for cmdline is good... plus tests.

---
+++ firebird/validate.py
@@ -0,0 +1,84 @@
+from firebird import dates as fd
+from firebird import functions
+from firebird import transforms
+import re
+
+def _acquired(a):
+    """Checks acquired date range as supplied by cmdline
+    :param a: Date range string in iso8601 format separated by /
+    :return: True if ok, Exception otherwise.
+    """
+    if not fd.is_acquired(a):
+        raise Exception("Invalid acquired dates:{}".format(a))
+    return True
+
+
+def _clip(c):
+    """Checks clip parameter as supplied by cmdline
+    :param c: Clip parameter
+    :return: True or Exception
+    """
+    if functions.false(c) or functions.true(c):
+        return True
+    raise Exception("Clip must be true or false")
+
+
+def _bounds(b):
+    """Checks bounds as supplied by cmdline
+    :param b: ('123,456', '222,333')
+    :return: True or Exception
+    """
+    if (len(b) > 0 and
+        all(map(lambda x: x.find(',') != -1, b)) and
+        all(map(lambda x: functions.isnumeric(x),
+            functions.flatten(map(lambda i: i.split(','), b))))):
+        return True
+    raise Exception("Invalid bounds specified:".format(b))
+
+
+def _products(products):
+    """Checks products as supplied by cmdline
+    :param products: ('seglength', 'changemag',)
+    :return: True or Exception
+    """
+    available = transforms.labels().keys()
+    p = set(products)
+    unavailable = p - p.intersection(available)
+    if unavailable:
+        raise Exception("Invalid product(s):{}".format(unavailable))
+
+
+def _product_dates(acquired, product_dates):
+    """Checks product_dates as supplied by cmdline
+    :param acquired: Acquired date range from cmdline
+    :param product_dates: ('1980-01-01', '1985-10-1',)
+    :return:
+    """
+    def check(date):
+        if not re.match('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', date):
+            raise Exception("Invalid product date value: {}".format(date))
+        start = fd.to_ordinal(fd.startdate(acquired))
+        end = fd.to_ordinal(fd.enddate(acquired))
+        val = fd.to_ordinal(date)
+        if val < start or val > end:
+            raise Exception("Product date {} out of range{}".format(date,
+                                                                    acquired))
+
+    return [check(d) for d in product_dates]
+
+
+def save(acquired, bounds, products, product_dates, clip):
+    """Checks inputs for save() function (via cmdline)
+    :param acquired: iso8601 dates separated by /
+    :param bounds:
+    :param products:
+    :param product_dates:
+    :param clip:
+    :return: True or Exception
+    """
+    _acquired(acquired)
+    _bounds(bounds)
+    _clip(clip)
+    _products(products)
+    _product_dates(product_dates)
+    return True
