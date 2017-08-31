f318c9705749102ee19182ec894de2ba05bf3f93
davidvhill <davehill75@gmail.com>
2017-08-31 13:34:50 -0500

    updated firebird to work with merlin 1.0-rc3.  Tests are passing.
    Former-commit-id: 669fefe8c411bd9155b26e735decd5e7db455edb

--- firebird/ancillaries.py
+++ firebird/ancillaries.py
@@ -1,10 +1,10 @@
-from cytoolz.curried import compose, filter, identity, map, merge, partial, pipe
+from cytoolz import filter, identity, juxt, map, merge, partial, pipe, reduce
 from operator import add, mul
 from osgeo import gdal
 import functools
 
 
-def location(spec, yx):
+def locate(yx, spec):
     """Determines projection space location from array positions + spec
     :param spec: dict containing ulx, uly, pixel_x, pixel_y
     :param yx: sequence of y array position, x array position
@@ -12,8 +12,8 @@ def location(spec, yx):
     """
     return merge([spec, {'y_index': yx[0],
                          'x_index': yx[1],
-                         'y': spec['uly'] + (yx[0] * spec['pixel_y']),
-                         'x': spec['ulx'] + (yx[1] * spec['pixel_x'])}])
+                         'y': int(spec['uly'] + (yx[0] * spec['pixel_y'])),
+                         'x': int(spec['ulx'] + (yx[1] * spec['pixel_x']))}])
 
 
 def locations(spec):
@@ -21,9 +21,9 @@ def locations(spec):
     :param spec: dict containing xsize, ysize, pixel_x, pixel_y, ulx, uly keys
     :return: Generator yielding spec plus x, y, x_index and y_index keys
     """
-    locator = partial(location, spec=spec)
-    indices = ((y, x) for x in range(spec['xsize'])
-                      for y in range(spec['ysize']))
+    locator = partial(locate, spec=spec)
+    indices = ((y, x) for y in range(spec['ysize'])
+                      for x in range(spec['xsize']))
     return map(locator, indices)
 
 
@@ -52,36 +52,36 @@ def genspec(file, nodata=None, fill=None):
         ds = None
 
 
-def value(memarray, spec):
+def value(spec, memarray):
     """Extracts a value from a memory array
-    :param memarray: two dimensional data array
     :param spec: dict with y and x keys
+    :param memarray: two dimensional data array
     :return:  Value associated with y and x array positions
     """
-    return memarray[spec['y']][spec['x']]
+    return memarray[spec['y_index']][spec['x_index']]
 
 
-def compact(spec):
+def compact(specs):
     """Excludes nodata and fill values from the dataset
     :param spec: A spec with value, nodata and fill keys
     :return: A filtered spec excluding nodata and fill
     """
-    return partial(filter, lambda spec: spec['value'] not in (spec['nodata'],
-                                                              spec['fill']))
+    return filter(lambda s: s['value'] not in (s['nodata'], s['fill']), specs)
 
 
-def read(filepath, locations):
+def read(specs, filepath):
     """Reads values from file specified by locations
     :param filepath: Path to raster to read from
-    :param locations: sequence of dicts containing y_index, x_index, y, x
-    :return: lazy sequence of merged dicts with 'value' key populated
+    :param specs: sequence of dicts containing y_index, x_index, y, x
+    :return: Generated sequence of merged spec dicts with 'value' key populated
     """
     ds = None
     try:
         ds = gdal.Open(filepath)
-        ma = ds.GetRasterBand(1).GetVirtualMemArray()
-        reader = merge(juxt(identity, lambda spec: {'value': value(ma, spec)}))
-        return map(reader, locations)
+        val = partial(value, memarray=ds.GetRasterBand(1).GetVirtualMemArray())
+        reader = juxt([identity, lambda spec: dict(value=val(spec=spec))])
+        for spec in specs:
+            yield (merge(reader(spec)))
     finally:
         ds = None
 
@@ -94,40 +94,43 @@ def csv(specs):
     return map(lambda spec: '{x},{y},{value}\n'.format(**spec), specs)
 
 
-def write(filepath, values):
-    """Creates a new filewriter for the file named by filepath
+def write(values, filepath):
+    """Writes a sequence of values to a filepath
     :param filepath: Path, name and extension of file to write to
     :param values: Sequence of values to write
     :return: Sequence of results from handle.write(value) function calls
     """
     with open(filepath, 'w+') as handle:
-        return map(handle.write(value), values)
+        return (filepath, reduce(add, map(handle.write, values)))
 
 
-def tif_to_json(tifpath, outpath):
+def tif_to_json(tifpath, outpath, nodata=None, fill=None, partition_size=10000):
     """Converts TIF files to JSON suitable for reading as DataFrames
     :param tifpath: Full path to input tif file
     :param jsonpath: Full path to output json file
+    :param fill: Fill value.  Used to exclude values from the conversion
     :param nodata: Nodata value.  Used to exclude values which shouldnt be
                    included in the conversion.
+    :param partition_size: How big should each file partition be
     :return: Full path of output json file
     """
-    filewriter = writer(outpath)
+    reader = partial(read, filepath=tifpath)
+    writer = partial(write, filepath=outpath)
 
     return pipe(tifpath,
                 genspec,
                 locations,
-                partial(read, filepath=tifpath),
+                reader,
                 compact,
                 csv,
-                partial(write, filepath=outpath))
+                writer)
 
 
-def ingest(jsonpath, spec, table=None, bbox=None):
+def ingest(csvpath, spec, table=None, bbox=None):
     """Ingests ancillary data located at path, specified by spec and constrained
     by bbox.
     :param path: Path to the json ancillary file
-    :param spec: spec for ancillary data containing pixel size, no data
+    :param spec: spec for ancillary data containing pixel size, nodata, fill
     :param name: Name to use for DataFrame table.  If None, derive from jsonpath
                  filename.
     :param bbox: dict() keyed by ulx, uly, lrx, lry.  Only ingest data points
