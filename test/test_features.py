ea9fdaa51dd25e4b883a7cece5212b12d6cdc4f5
clay austin <clay.austin.ctr@usgs.gov>
2018-05-14 08:40:42 -0500

    add features and randomforest test placeholders

---
+++ test/test_features.py
@@ -0,0 +1,16 @@
+from firebird import features
+
+def test_join():
+    assert True
+
+def test_columns():
+    assert True
+
+def test_dependent():
+    assert True
+
+def test_independent():
+    assert True
+
+def test_dataframe():
+    assert True
