d8f8d742a95fee5887cc07f87108bc7d24919b91
clay austin <clay.austin.ctr@usgs.gov>
2018-06-05 08:49:40 -0500

    remove monkeypatching from timeseries tests

--- test/test_timeseries.py
+++ test/test_timeseries.py
@@ -1,5 +1,7 @@
-from firebird    import ARD
-from firebird    import timeseries
+from cytoolz  import first
+from cytoolz  import second
+from firebird import ARD
+from firebird import timeseries
 
 from .shared import acquired
 from .shared import ard_schema
@@ -29,27 +31,21 @@ def test_dataframe(spark_context):
     dframe = timeseries.dataframe(spark_context, rdd)
     assert type(dframe) is pyspark.sql.dataframe.DataFrame
 
-def test_rdd(spark_context, monkeypatch, ids_rdd):
-    monkeypatch.setattr(merlin, 'create', mock_merlin_create)
-    ard   = timeseries.rdd(ctx=spark_context, cids=ids_rdd, acquired=acquired, cfg=ARD, name='ard')
-    first = list(ard.map(lambda x: x).collect())
-
+def test_rdd(spark_context, ids_rdd, merlin_ard_config):
+    ard   = timeseries.rdd(ctx=spark_context, cids=ids_rdd, acquired=acquired, cfg=merlin_ard_config, name='ard')
+    results = ard.map(lambda x: x).collect()
     assert type(ard) is pyspark.rdd.RDD
-    assert first == [((11, 22, 33, 44), (-1815585.0, 1064805.0)), 
-                     ((11, 22, 33, 44), (-1815585.0, 1061805.0)),
-                     ((11, 22, 33, 44), (-1815585.0, 1058805.0))]
+    assert type(first(results)) is tuple #((11, 22, 33, 44), (-1815585.0, 1064805.0)) 
+    assert len(first(results)) == 2
+    assert type(first(results)[1][0]) is float
 
-def test_ard(spark_context, monkeypatch, ids_rdd):
-    monkeypatch.setattr(timeseries, 'rdd', mock_timeseries_rdd)
-    ard_df = timeseries.ard(spark_context, ids_rdd, acquired)
-
+def test_ard(spark_context, ids_rdd, merlin_ard_config):
+    ard_df = timeseries.ard(spark_context, ids_rdd, acquired, cfg=merlin_ard_config)
     assert type(ard_df) is pyspark.sql.dataframe.DataFrame
     assert ard_df.columns == ard_schema
 
-def test_aux(spark_context, monkeypatch, ids_rdd):
-    monkeypatch.setattr(timeseries, 'rdd', mock_timeseries_rdd)
-    aux_df = timeseries.aux(spark_context, ids_rdd, acquired)
-
+def test_aux(spark_context, ids_rdd, merlin_aux_config):
+    aux_df = timeseries.aux(spark_context, ids_rdd, acquired, cfg=merlin_aux_config)
     assert type(aux_df) is pyspark.sql.dataframe.DataFrame
     assert set(aux_df.columns) == set(aux_schema)
     
