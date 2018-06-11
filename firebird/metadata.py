66c218c35fc58668484553cb23155118f239aba0
davidvhill <davehill75@gmail.com>
2018-06-11 13:03:12 -0500

    update metadata.py for checking prior to rename to ccdc

--- firebird/metadata.py
+++ firebird/metadata.py
@@ -10,13 +10,14 @@ from merlin.functions import denumpify
 from pyspark import sql
 from pyspark.sql import SparkSession
 from pyspark.sql import functions
-from pyspark.sql.types import ArrayType
-from pyspark.sql.types import ByteType
-from pyspark.sql.types import FloatType
+from pyspark.sql.types import DateType
 from pyspark.sql.types import IntegerType
+from pyspark.sql.types import MapType
 from pyspark.sql.types import StringType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import StructType
+
+import datetime
 
 
 def table():
@@ -26,15 +27,63 @@ def table():
 
 
 def schema():
+    """pyspark dataframe schema for metadata"""
+    
     return StructType([
-        StructField('tilex' , IntegerType(), nullable=False),
-        StructField('tiley' , IntegerType(), nullable=False),
-        StructField('ddate' , DateType(),    nullable=False),
-        StructField('cdate' , DateType(),    nullable=True),
-        StructField('dtype' , StringType(),  nullable=False),
-        StructField('ctype' , StringType(),  nullable=True),
-        StructField('ardurl', IntegerType(), nullable=False),
-        StructField('auxurl', IntegerType(), nullable=False)])
+        StructField('tilex', IntegerType(), nullable=False),
+        StructField('tiley', IntegerType(), nullable=False),
+        StructField('detector', StringType(), nullable=True),
+        StructField('detector_ran', DateType(), nullable=True),
+        StructField('detector_params', MapType(StringType(), StringType()), nullable=True),
+        StructField('classifier', StringType(), nullable=True),
+        StructField('classifier_ran', DateType(), nullable=True),
+        StructField('classifier_params', MapType(StringType(), StringType()), nullable=True),
+        StructField('ardurl', IntegerType(), nullable=True),
+        StructField('auxurl', IntegerType(), nullable=True)])
+
+
+def detector(tilex, tiley, detector, params, ardurl, auxurl):
+    """create metadata for detectors
+
+    Args:
+        tilex: x coordinate of tile
+        tiley: y coordinate of tile
+        detector: name and version of detector
+        params: parameters supplied to detector at runtime
+        ardurl: URL used to supply input ARD data to detector
+        auxurl: URL used to supply input AUX data to detector
+
+    Returns:
+        dict: Detector metadata
+    """
+    
+    return {'tilex': tilex,
+            'tiley': tiley,
+            'detector': detector,
+            'detector_ran': datetime.datetime.now().isoformat(),
+            'detector_params': params,
+            'ardurl': ardurl,
+            'auxurl': auxurl}
+
+
+def classifier(tilex, tiley, classifier, params):
+    """create metadata for classifiers
+
+    Args:
+        tilex: x coordinate of tile
+        tiley: y coordinate of tile
+        detector: name and version of classifier
+        params: parameters supplied to classifier at runtime
+
+    Returns:
+        dict: Classifier metadata
+    """
+
+    return {'tilex': tilex,
+            'tiley': tiley,
+            'classifier': classifier,
+            'classifier_ran': datetime.datetime.now().isoformat(),
+            'classifier_params': params}
 
 
 def dataframe(ctx, rdd):
@@ -52,41 +101,6 @@ def dataframe(ctx, rdd):
     return SparkSession(ctx).createDataFrame(rdd, schema())
  
        
-def detect(timeseries):
-    """Takes in a timeseries and returns a list of detections
-     
-    Args:
-        timeseries (dict): ard timeseries
-
-    Return:
-        sequence of change detections
-    """
-
-    chipx, chipy, x, y = first(timeseries)
-
-    return format(chipx=chipx,
-                  chipy=chipy,
-                  x=x,
-                  y=y,
-                  dates=get('dates', second(timeseries)),
-                  ccdresult=ccd.detect(**second(timeseries)))
-
-
-#def rdd(ctx, timeseries):
-#    """Run pyccd against a timeseries
-#
-#    Args:
-#        ctx: spark context
-#        timeseries: RDD of timeseries data
-#
-#    Returns:
-#        RDD of pyccd results
-#    """
-#    
-#    logger(context=ctx, name=__name__).info('executing change detection...')
-#    return timeseries.flatMap(detect)
-
-
 def read(ctx, ids):
     """Read metadata results
 
